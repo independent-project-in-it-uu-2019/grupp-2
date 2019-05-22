bd331376758d4df52173cd175f19f2c29e546213
masm3948 <masm3948@student.uu.se>
2019-05-22 15:47:13 +0200

    började med sortering av kurser programvis

--- app.js
+++ app.js
@@ -5,15 +5,95 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+const rp = require('request-promise');
+const c = require('cheerio');
 
 var soap = require('soap');
 var apiWSDL = 'http://test-ws.selma7.its.uu.se/selmaws-uu/services/PlanTjanst?wsdl';
 
+const webScrapeUrl = 'http://www.uu.se/utbildning/utbildningar/selma/studieplan/?pKod=';
+
+var selmaSearchResult = {};
+
+var kurskoder = {
+    TEL2Y: [],
+    TES2Y: [],
+    TTF2Y: [],
+    TIT2Y: [],
+    TKT2Y: [],
+    TTM2Y: [],
+    TST2Y: [],
+    TMV2Y: [],
+    TMB2Y: [],
+    TEL1Y: [],
+    TME1Y: [],
+    TDV1K: []
+}
+
+function populatekurskoder() {
+    for (let kurs in kurskoder) {
+
+        let url = webScrapeUrl + kurs;
+
+        rp(url)
+            .then((html) => {
+
+                //success!
+                const courses = [];
+                var html = c('ul.semesterContent > li > a', html);
+                for (let i = 0; i < html.length; i++) {
+                    let str = html[i].children[0].data;
+                    let slices = str.split("(");
+                    let code = slices[slices.length-1].slice(0, 6);
+                    courses.push(code);
+                }
+                kurskoder[kurs] = courses;
+            })
+            .catch(function (err) {
+                //handle error
+                //console.log(err);
+            });
+    }
+}
+
+populatekurskoder();
+
+function filterProgram(programCode) {
+    let coursecodes = kurskoder[programCode];
+    let res = {};
+
+    for (let i = 0; i < coursecodes.length; i++) {
+        if (coursecodes[i] in selmaSearchResult) {
+            res[coursecodes[i]] = selmaSearchResult[coursecodes[i]];
+        }
+    }
+    selmaSearchResult = res;
+};
+
+io.on('connection', function (socket) {
+    async function search(data) {
+        selmaSearchResult = await getKursplan(data, '');
+        socket.emit('searchResult', selmaSearchResult);
+    }
+
+    socket.on('search', data => {
+        if (lengthOfObject(selmaSearchResult) < 1) search(data);
+        else {
+            filterProgram('TIT2Y');
+            socket.emit('searchResult', selmaSearchResult);
+        }
+    })
+
+    socket.on('filterProgram', code => {
+        filterProgram(code);
+        socket.emit('searchResult', selmaSearchResult);
+    })
+});
+
+
 // Hur skiljer vi på kurskod och kursnamn?
 // Hur ska vi begränsa antalet resultat, typ att man måste ha minst tre 
 // bokstäver/siffror i söktermen? 
-
-var kurser = [];
 
 function getKursplan(benamning, kurskod) {
     console.log('Searching...');
@@ -62,19 +142,6 @@ function getKursplan(benamning, kurskod) {
 }
 //getKursplan('', '1DT350');
 
-io.on('connection', function (socket) {
-    socket.emit('initialize', 'Hej klient!');
-    async function search(data) {
-        var kurser = await getKursplan(data, '');
-        console.log(kurser);
-        socket.emit('searchResult', kurser);
-    }
-
-    socket.on('search', data => {
-        search(data);
-    })
-});
-
 // Pick arbitrary port for server
 var port = 3000;
 app.set('port', (process.env.PORT || port));
@@ -105,3 +172,11 @@ app.get('/login', function (req, res) {
 var server = http.listen(app.get('port'), function () {
     console.log('Server listening on port ' + app.get('port'));
 });
+
+function lengthOfObject(obj) {
+    let n = 0;
+    for (let o in obj) {
+        n++;
+    }
+    return n;
+}
